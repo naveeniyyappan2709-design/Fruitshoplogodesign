fa910b32c3bd070178ce807a4601292f37a066c2
naveenkumar <naveeniyyappan2709@gmail.com>
2026-04-08 16:46:46 +0530

    version3.o

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
